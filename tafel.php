827f2f0ae7c4add4f7bbb1a68adce04967744bf8
daanberg <6013014@mborijnland.nl>
2021-05-18 13:30:21 +0200

    Tafel pagina aangemaakt

---
+++ tafel.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Daan Berg">
+    <meta name="keywords" content="">
+    <title>Tafels | Mathmate</title>
+
+    <link rel="stylesheet" type="text/css" href="CSS/tafels.css">  
+</head>
+<body>
+    
+</body>
+</html>
