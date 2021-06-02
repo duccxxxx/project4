21947dfef04e97bdcccfcf095c3bd79da97e3731
oskar kwiatek <77640637+duccxxxx@users.noreply.github.com>
2021-06-02 20:49:54 +0200

    Add files via upload

--- presentatiepagina.php
+++ presentatiepagina.php
@@ -17,7 +17,7 @@
         <nav>
              <section id="navbar">
                     <ul>
-                        <li><a href="">Home</a></li>
+                        <li><a href="Homepage.php">Home</a></li>
                         <li><a href="">Onderwerpen</a></li>
                         <li><img src="img/thereal.png" height="100vh"></li>
                         <li><a href="">Game</a></li>
@@ -31,20 +31,20 @@
 
     <div class="mySlides fade">
     <div class="numbertext">1 / 3</div>
-    <img src="img/logo periode 4.png" style="width:100%">
-    <div class="text">Mathmate</div>
+    <img src="img/thereal1.png" style="width:100%">
+    <div class="text"></div>
     </div>
 
     <div class="mySlides fade">
     <div class="numbertext">2 / 3</div>
     <img src="img/thereal.png" style="width:100%">
-    <div class="text">Rekenpagina</div>
+    <div class="text"></div>
     </div>
 
     <div class="mySlides fade">
     <div class="numbertext">3 / 3</div>
     <img src="img/thereal.png" style="width:100%">
-    <div class="text">Onderwerpen</div>
+    <div class="text"></div>
     </div>
 
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -59,7 +59,8 @@
     <span class="dot" onclick="currentSlide(3)"></span> 
     </div>    
 
-	
+	<script src="css/slideshow.js">
+    </script>
 
     <footer>
         
---
+++ rekenpagina2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=""> 
+    <meta name="author" content="Kevin Xue">
+    <meta name="keywords" content=" ">
+    <title>Homepagina</title>
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header id="nav-bar">
+            <nav>
+                <section id="navbar">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Onderwerpen</a></li>
+                            <li><img src="img/thereal.png" height="100vh"></li>
+                            <li><a href="">Game</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                </section>
+            </nav>
+    </header>
+
+
+    <footer>
+        <section id="footer">
+            <p><b>Contactgegevens</b><br>Email: info@mathmate.nl<br>Tel: 070 342 23 65</p>
+        </section>
+        <section id="footer2">
+            <a href="homepage.php"><img src="img/logoperiode4.png" height="250vh"></a>
+        </section>
+        <section id="socials">
+            <a href="https://www.instagram.com/daily_math_/"><img src="img/instagram.png" height="75vh"></a>
+            <a href="https://www.twitter.com/"><img src="img/twitter.png" height="75vh"></a>
+            <a href="https://www.facebook.com/"><img src="img/facebook2.png" height="75vh"></a>
+        </section>
+    </footer>
+</body>
+</html>
